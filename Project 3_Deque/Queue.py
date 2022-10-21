aa36a70e713de001b1d5d65b71cb5f0cb7641d1b
CTlandu <jizhoutang@outlook.com>
2022-10-21 18:03:44 -0400

    add project 3 empty

---
+++ Project 3_Deque/Queue.py
@@ -0,0 +1,32 @@
+from Deque_Generator import get_deque
+
+class Queue:
+
+  def __init__(self):
+    self.__dq = get_deque()
+
+  def __str__(self):
+    # TODO replace pass with your implementation.
+    # Orient your string from front (left) to back (right).
+    pass
+
+  def __len__(self):
+    # TODO replace pass with your implementation.
+    pass
+
+  def enqueue(self, val):
+    # TODO replace pass with your implementation.
+    pass
+
+  def dequeue(self):
+    # TODO replace pass with your implementation.
+    pass
+
+  def peek(self):
+    # TODO replace pass with your implementation.
+    pass
+
+# Unit tests make the main section unneccessary.
+#if __name__ == '__main__':
+#  pass
+  
---
+++ Project 3_Deque/Stack.py
@@ -0,0 +1,31 @@
+from Deque_Generator import get_deque
+
+class Stack:
+
+  def __init__(self):
+    self.__dq = get_deque()
+
+  def __str__(self):
+    # TODO replace pass with your implementation.
+    # Orient your string from top (left) to bottom (right).
+    pass
+
+  def __len__(self):
+    # TODO replace pass with your implementation.
+    pass
+
+  def push(self, val):
+    # TODO replace pass with your implementation.
+    pass
+
+  def pop(self):
+    # TODO replace pass with your implementation.
+    pass
+
+  def peek(self):
+    # TODO replace pass with your implementation.
+    pass
+
+# Unit tests make the main section unneccessary.
+#if __name__ == '__main__':
+#  pass
